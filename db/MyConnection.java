4202e5457d450e26e827eef37f20f32bad3d7364
Ayush <ayushcharde933@gmail.com>
2024-01-16 08:46:56 +0530

    first commit

---
+++ db/MyConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HiderSpace?useSSL=false", "root", "Database-root644");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection build Successfully !!!");
+        return connection;
+    }
+    public static void closeConnection() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+}
